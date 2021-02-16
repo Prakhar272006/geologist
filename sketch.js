186b9e4d9e6391620e8891bf4f1278e594d901cc
Prakhar272006 <77096052+Prakhar272006@users.noreply.github.com>
2021-02-16 12:10:44 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,56 @@
-const Engine =  Matter.Engine
-const World =  Matter.World
-const Bodies =  Matter.Bodies
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
 
-var engine ,world ,ball,ground; 
+var engine, world;
+var stone, iron,rubber,hammer;
+var sand1, sand2, sand3, sand4,sand5,sand6;
 
-function setup() {
-  createCanvas(1500,700);
- engine = Engine.create();
- world = engine.world
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
 
-stone1 = new stone(900,600,100,100)
+    plane = new Plane(600,height,1200,20)
+    
+    stone = new Stone(700,320,100,100);
+    iron = new Iron(300,350);
+    rubber=new Rubber(900,450,70);
+    hammer = new Hammer(10,100);
 
-
-rubber1 = new rubber(500,450,)
-
-//log1 = new  log(775,500,200,PI/2)
-
-
-hammer1 = new hammer(100,100)
-
-ground = new Ground(width/2,height,width,25)
+    sand1 = new Sand(505,450,10);
+    sand2 = new Sand(510,450,10);
+    sand3 = new Sand(515,450,10);
+    sand4 = new Sand(520,450,10);
+    sand5 = new Sand(525,450,10);
+    sand6 = new Sand(515,445,10);
+    sand7 = new Sand(520,445,10);
+    sand8 = new Sand(525,445,10);
+    sand9 = new Sand(530,445,10);
+    sand10 = new Sand(535,445,10);
 
 
 }
 
-function draw() {
-  background("lightblue");  
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
 
-  Engine.update(engine)
-  stone1.display()
-  rubber1.display()
+    stone.display();
+    plane.display();
+    iron.display();
+    rubber.display();
 
-  hammer1.display()
-  ground.display()
+    hammer.display();
+
+    sand1.display();
+    sand2.display();
+    sand3.display();
+    sand4.display();
+    sand5.display();
+    sand6.display();
+    sand7.display();
+    sand8.display();
+    sand9.display();
+    sand10.display();
 }
